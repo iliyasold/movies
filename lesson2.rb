4256b162e375ff14d53338a4f87adf736ff573a7
iliyasold <iliyasold@gmail.com>
2017-09-28 21:44:57 +0300

    File name issued in command line #2

--- lesson2.rb
+++ lesson2.rb
@@ -1,11 +1,11 @@
+puts File.basename("movies.txt")
 
 File.foreach('movies.txt') do |line|
   if line.include?('Max')
     a = line.split('|')
     b = (((a[7].to_f - 8).round(2))*10).to_i
       if a[1].include?('Max')
-        puts "#{a[1]} #{"*" * b} #{File.basename('movies.txt')}"
+        puts "#{a[1]} #{"*" * b} #{}"
       end
   end
 end
-
