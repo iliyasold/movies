0db42ec590d5ef5fe679bc4f060ded5d176c1ceb
iliyasold <iliyasold@gmail.com>
2017-09-28 18:39:39 +0300

    Script for choice movies from file with view it raiting

---
+++ lesson2.rb
@@ -0,0 +1,10 @@
+
+File.foreach('movies.txt') do |line|
+  if line.include?('Max')
+    a = line.split('|')
+    b = (((a[7].to_f - 8).round(2))*10).to_i
+      if a[1].include?('Max')
+        puts "#{a[1]} #{"*" * b}"
+      end
+  end
+end
