c5e2f5ea1552952ec6a595166f945989d079c052
iliyasold <iliyasold@gmail.com>
2017-09-29 14:27:36 +0300

    Added the possibility to use the given file

--- lesson2.rb
+++ lesson2.rb
@@ -6,10 +6,14 @@ end
 
 puts ARGV[0]
 
-File.foreach('movies.txt') do |line|
-
+File.foreach(ARGV[0]) do |line|
+	
   a = line.split('|')
   b = (((a[7].to_f - 8).round(2))*10).to_i
-
-  puts "#{a[1]} #{"*" * b} #{}" if a[1].include?('Max')
+	  
+  if a[1] == nil
+  	abort "Sorry, file #{ARGV[0]} doesn't have required data"
+  else
+    puts "#{a[1]} #{"*" * b} #{}" if a[1].include?('Max')
+  end
 end
