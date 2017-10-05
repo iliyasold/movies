130fe0d271436fa36ee8513c09f78111449d08d4
iliyasold <iliyasold@gmail.com>
2017-10-05 18:04:21 +0300

    Intermediate result

--- les.rb
+++ les.rb
@@ -2,25 +2,28 @@
 
 file = ARGV[0] || 'movies.txt'
 
-unless File.exists?(file)
-  abort "Sorry, file #{file} not found."
-end
+  unless File.exists?(file)
+    abort "Sorry, file #{file} not found."
+  end
 
-puts file
+    puts file
 
-directors = []
-timing = []
-countries = []
+    directors = [] 
+    timing = []
+    movieslib = {}
+    countries = []
+    # fivelongest
+    tenfirst_arr = []
 
 File.foreach(file) do |line|
 
-  a = line.split('|')
-  b = (((a[7].to_f - 8).round(2))*10).to_i
-  puts "#{a[1]} #{"*" * b} #{}" if a[1].include?('Max')
+    a = line.split('|')
+    b = (((a[7].to_f - 8).round(2))*10).to_i
+    puts "#{a[1]} #{"*" * b} #{}" if a[1].include?('Max')
 
-# lesson_3
+  # lesson_3
 
-# Сохранение в хэш
+  # Сохранение в хэш
   movieslib = { 
   	link: a[0], 
   	title: a[1], 
@@ -34,26 +37,43 @@ File.foreach(file) do |line|
   	actors: a[9] 
   }
 
-  directors << movieslib[:director] 
-  timing << movieslib[:timing]
-  countries << movieslib[:country]
+    directors << movieslib[:director] 
+    timing << movieslib[:timing]
+    countries << movieslib[:country]
+    
+    fivelongest = {
+    	title: a[1], 
+    	timing: a[6]
+    }
+    
+    tenfirst = {
+    	title: a[1], 
+    	genre: a[5], 
+    	date: a[4]
+    }
+
+    tenfirst_arr << tenfirst.each { |item| }
 end
-
-# Удаление дублей и сортировка режиссёров по фамилии в алфавитном порядке 
-  array = [] 
-  directors.each { |item| array << item.split.reverse.join(' ') } 
-  puts array.sort.uniq
-
-  # Количество фильмов, снятых не в США
-  puts (countries.delete_if {|x| x == "USA" }).size
-
   
+  # 5 самых длинных фильмов 
+  # :title & :timing
 
-  # # Поиск пяти самых длинных фильмов
-#   array2 = []
-#   timing.each { |item| array2 << item[0..-5] }
+  # 10 комедий вышедших раньше остальных
+    tenfirst_arr.each do |item|
+      if item[:genre].include?("Comedy") # и первые 10 самых ранних дат
+      	 puts "#{item[:title]} - #{item[:genre]} - #{item[:date]}"
+      end
+    end
 
-#   array3 = []
-#   array2.each { |item| array3 << item.to_i }	
-#   puts array3.sort[-5..-1]
+  # Удаление дублей и сортировка режиссёров по фамилии в алфавитном порядке 
+	array = [] 
+	directors.each { |item| array << item.split.reverse.join(' ') } 
+	puts array.sort.uniq
 
+  # Количество фильмов снятых не в США
+  def print_block
+  	without_US = yield
+  	puts "#{without_US} films are not made in the USA"
+  end
+
+  	print_block { (countries.delete_if {|x| x == "USA" }).size }
